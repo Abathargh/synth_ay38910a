c5bacb8cd9424b605e8b29053517252185c01b14
mar <gim.marcello@gmail.com>
2021-10-10 19:20:23 +0200

    added: complete implementation of the AY38910A drivers

--- AY38910A-uSynth/lib/Ay38910a/Ay38910a.c
+++ AY38910A-uSynth/lib/Ay38910a/Ay38910a.c
@@ -38,20 +38,50 @@
  *
  * OCR2VAL = T_clk/T_del - 1 = (0.0625 us / 0.25 us) - 1 = 3 
  */
-#define OCR2AVAL 3
+#define OCR2AVAL       3
 
-#define BC1      7
-#define BDIR     6
+#define BC1            7
+#define BDIR           6
+
+#define NOISE_REG      0x06
+#define MIXER_REG      0x07
+#define FINE_ENV_REG   0x0B
+#define COARSE_ENV_REG 0x0C
+#define SHAPE_ENV_REG  0x0D
+
+#define CHAN_TO_AMP_REG(c) (((u8)c / 2) + 8)
+
 
 /************************************************************************/
 /* Private function declarations                                        */
 /************************************************************************/
 
 static void clock_init(void);
+static void inactive_mode(void);
+static void latch_address_mode(void);
+static void write_mode(void);
+static void write_to_data_bus(u8 address, u8 data);
 
 /************************************************************************/
 /* Private variables                                                    */
 /************************************************************************/
+
+/**
+ * The AY38910A tone generation works in the following way: the clock 
+ * signal gets scaled by a factor of 16 and by an additional factor 
+ * depending on the value contained in the 12 less significant bits of
+ * the two registers related to the channel that is being used.
+ *
+ * f_clk  = 2MHz
+ * f_high = f / 16 = 125 KHz          => Highest note programmable
+ * f_low  = (f / 16) / 2^12 ~ 30.5 Hz => Lowest note programmable
+ *
+ * This array contains some pre-computed magic numbers that correspond
+ * to real notes following the mathematical equation just introduced.
+ */
+static const u16 magic_notes[] = {
+	
+};
 
 /************************************************************************/
 /* Function implementations                                             */
@@ -68,6 +98,71 @@ void ay38910_init(void)
 	DDRC = 0xA0; // 10100000
 	
 	clock_init();
+}
+
+/**
+ * Plays a note on the specified channel
+ *
+ * @param chan the channel to program 
+ * @ note the note to play depending on the internal mapping (0 off)
+ */
+void ay38910_play_note(Channel chan, u8 note)
+{
+	u8 reg1, reg2;
+	
+	reg1 = (u8) chan;
+	reg2 = ((u8) chan) + 1;
+	
+	write_to_data_bus(reg1, 0xFF & magic_notes[note]);
+	write_to_data_bus(reg2, 0x0F & (magic_notes[note] >> 8));
+}
+
+/**
+ * Plays a sound on the noise channel
+ *
+ * @param divider the divider value to manipulate the noise
+ *
+ * @retval None
+ */
+void ay38910_play_noise(u8 divider)
+{
+	write_to_data_bus(NOISE_REG, 0x1F & divider);
+}
+
+/**
+ * Enables/Disables tone/noise generation on one or more channels
+ *
+ * @param mode the mode(s) to enable, using the ENABLE/DISABLE defines
+ *
+ * @retval None
+ */
+void ay38910_channel_mode(u8 mode)
+{
+	write_to_data_bus(MIXER_REG, mode);
+}
+
+/**
+ * Sets the amplitude for the specified channel
+ *
+ * @param chan the channel that will have the passed amplitude
+ * @param amplitude the amplitude to set, the first 4 bytes
+ *        refer to the amplitude itself (0-16), the fifth byte 
+ *        enables the envelope filter and disables the use of the 
+ *        first three bytes. Use with the enable/disable macros.
+ 
+ * @retval None
+ */
+void ay38910_set_amplitude(Channel chan, u8 amplitude)
+{
+	u8 amplitude_reg = CHAN_TO_AMP_REG(chan);
+	write_to_data_bus(amplitude_reg, 0x1F & amplitude);
+}
+
+void ay38910_set_envelope(EnvelopeShape shape, u16 frequency)
+{
+	write_to_data_bus(FINE_ENV_REG, 0xFF & frequency);
+	write_to_data_bus(COARSE_ENV_REG, 0xFF & (frequency >> 8));
+	write_to_data_bus(SHAPE_ENV_REG, 0x07 & ((u8)shape));
 }
 
 /************************************************************************/
@@ -103,9 +198,9 @@ static void clock_init(void)
  *
  * @retval None
  */
-static void inactive_mode(void)
+static inline void inactive_mode(void)
 {
-	PORTB = (0 << BC1) | (0 << BDIR); 
+	PORTC = (0 << BC1) | (0 << BDIR); 
 }
 
 /**
@@ -113,9 +208,9 @@ static void inactive_mode(void)
  *
  * @retval None
  */
-static void write_mode(void)
+static inline void write_mode(void)
 {
-	PORTB = (0 << BC1) | (1 << BDIR);
+	PORTC = (0 << BC1) | (1 << BDIR);
 }
 
 /**
@@ -123,7 +218,21 @@ static void write_mode(void)
  *
  * @retval None
  */
-static void latch_address_mode(void)
+static inline void latch_address_mode(void)
 {
-	PORTB = (1 << BC1) | (1 << BDIR);
+	PORTC = (1 << BC1) | (1 << BDIR);
+}
+
+static void write_to_data_bus(u8 address, u8 data)
+{
+	// Set the register address
+	inactive_mode();
+	PORTA = address;
+	latch_address_mode();
+	inactive_mode();
+	
+	//Write to the previously set register
+	write_mode();
+	PORTA = data;
+	inactive_mode();	
 }
--- AY38910A-uSynth/lib/Ay38910a/Ay38910a.h
+++ AY38910A-uSynth/lib/Ay38910a/Ay38910a.h
@@ -15,24 +15,68 @@
 #include "types.h"
 
 /************************************************************************/
+/* Defines                                                              */
+/************************************************************************/
+
+#define CHA_TONE  0
+#define CHB_TONE  1
+#define CHC_TONE  2
+#define CHA_NOISE 3
+#define CHB_NOISE 4
+#define CHC_NOISE 5
+
+#define ENVELOPE_ENABLE 4
+
+#define CHA_TONE_ENABLE  (1 << CHA_TONE)
+#define CHA_TONE_DISABLE (0 << CHA_TONE)
+
+#define CHB_TONE_ENABLE  (1 << CHB_TONE)
+#define CHB_TONE_DISABLE (0 << CHB_TONE)
+
+#define CHC_TONE_ENABLE  (1 << CHC_TONE)
+#define CHC_TONE_DISABLE (0 << CHC_TONE)
+
+#define CHA_TONE_ENABLE  (1 << CHA_TONE)
+#define CHA_TONE_DISABLE (0 << CHA_TONE)
+
+#define CHA_NOISE_ENABLE  (1 << CHA_NOISE)
+#define CHA_NOISE_DISABLE (0 << CHA_NOISE)
+
+#define CHB_NOISE_ENABLE  (1 << CHB_NOISE)
+#define CHB_NOISE_DISABLE (0 << CHB_NOISE)
+
+#define CHC_NOISE_ENABLE  (1 << CHC_NOISE)
+#define CHC_NOISE_DISABLE (0 << CHC_NOISE)
+
+#define AMPL_ENVELOPE_ENABLE  (1 << ENVELOPE_ENABLE)
+#define AMPL_ENVELOPE_DISABLE (0 << ENVELOPE_ENABLE)
+
+/************************************************************************/
 /* Typedefs                                                             */
 /************************************************************************/
 
-typedef struct {
-	u8 data_pin;
-	u8 latch_pin;
-	u8 clock_pin;
-	u8 pin_BC1;
-	u8 pin_BCDIR;
-	u8 freq_output_pin;
-} ay_pin_config;
+typedef enum {
+	CHANNEL_A = 0x00,
+	CHANNEL_B = 0x02,
+	CHANNEL_C = 0x04
+} Channel;
 
+typedef enum {
+	SHAPE_HOLD      = 0x01,
+	SHAPE_ALTERNATE = 0x02,
+	SHAPE_ATTACK    = 0x04,
+	SHAPE_CONTINUE  = 0x08
+} EnvelopeShape;
 
 /************************************************************************/
 /* Public functions                                                     */
 /************************************************************************/
 
-void ay38910_init(ay_pin_config conf);
-
+void ay38910_init(void);
+void ay38910_play_note(Channel chan, u8 note);
+void ay38910_play_noise(u8 sound);
+void ay38910_channel_mode(u8 mode);
+void ay38910_set_amplitude(Channel chan, u8 amplitude);
+void ay38910_set_envelope(EnvelopeShape shape, u16 frequency);
 
 #endif /* AY38910A_H_ */
