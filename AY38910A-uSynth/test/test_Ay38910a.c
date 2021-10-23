72942855a53668e641b4d26f8c258912e4754099
mar <gim.marcello@gmail.com>
2021-10-23 23:35:04 +0200

    added: unit test infrastructure

---
+++ AY38910A-uSynth/test/test_Ay38910a.c
@@ -0,0 +1,22 @@
+#ifdef TEST
+
+#include "unity.h"
+
+#include "Ay38910a.h"
+
+#include "mock_io.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_Ay38910a_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement Ay38910a");
+}
+
+#endif // TEST
