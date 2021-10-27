f86ace9afe239acd478d58dae78bcf36d697c528
mar <gim.marcello@gmail.com>
2021-10-27 23:47:45 +0200

    changed: added access macro funcs implementation

--- AY38910A-uSynth/inc/pin_config.h
+++ AY38910A-uSynth/inc/pin_config.h
@@ -16,15 +16,13 @@
 #define LED_PIN PORTB,7
 
 #define INIT_PIN(PORT,PIN)   do{ DDRB |= (1 << PIN); } while(0)
+#define INIT_OUT_PORT(PORT) (PORT = 0xFF);
 
-#define GET_PIN(PORT,PIN)    (PORT	 & (1 << PIN))
-#define SET_PIN(PORT,PIN)    1
-#define RST_PIN(PORT,PIN)	 1
-#define TOGGLE_PIN(PORT,PIN) do { PORT ^= ((1 << PIN)); } while(0)
+#define GET_PIN(PORT,PIN)    (PORT &   (1 << PIN))
+#define SET_PIN(PORT,PIN)    (PORT |=  (1 << PIN))
+#define RST_PIN(PORT,PIN)	 (PORT &= ~(1 << PIN))
 
+#define 
 
-#define InitPinOuput(x) INIT_PIN(x)
-#define TogglePin(x) TOGGLE_PIN(x)
-
-
+// define here the pins to use and set them in the main/init so that ay module does not refer to io arv
 #endif /* PIN_CONFIG_H_ */
