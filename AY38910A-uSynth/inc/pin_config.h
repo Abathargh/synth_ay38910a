6fd426b400c86293d52e374ce453863961020b8a
mar <gim.marcello@gmail.com>
2021-10-29 20:16:11 +0200

    changed: finished implementation of the main macro functions related to the pin/port operations

--- AY38910A-uSynth/inc/pin_config.h
+++ AY38910A-uSynth/inc/pin_config.h
@@ -15,14 +15,11 @@
 
 #define LED_PIN PORTB,7
 
-#define INIT_PIN(PORT,PIN)   do{ DDRB |= (1 << PIN); } while(0)
-#define INIT_OUT_PORT(PORT) (PORT = 0xFF);
+#define INIT_OUT_PIN(PORT,PIN)  do{ DDR ## PRT |= (1 << PIN); } while(0)
+#define INIT_OUT_PORT(PRT)      (DDR ## PRT = 0xFF);
 
-#define GET_PIN(PORT,PIN)    (PORT &   (1 << PIN))
-#define SET_PIN(PORT,PIN)    (PORT |=  (1 << PIN))
-#define RST_PIN(PORT,PIN)	 (PORT &= ~(1 << PIN))
+#define GET_PIN(PRT,PIN)        (PRT ## PRT &   (1 << PIN))
+#define SET_PIN(PRT,PIN)        (PRT ## PRT |=  (1 << PIN))
+#define RST_PIN(PRT,PIN)        (PRT ## PRT &= ~(1 << PIN))
 
-#define 
-
-// define here the pins to use and set them in the main/init so that ay module does not refer to io arv
 #endif /* PIN_CONFIG_H_ */
