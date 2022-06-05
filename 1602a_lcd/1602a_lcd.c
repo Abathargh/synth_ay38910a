8b49fa40da8e69864d77a62cd153b83ecee5e81d
mar <gim.marcello@gmail.com>
2022-06-05 20:20:33 +0200

    changed: moved the command defines within the implementation source; added the support for setting the cursor position and to clear and print on a specific row.

--- 1602a_lcd/1602a_lcd.c
+++ 1602a_lcd/1602a_lcd.c
@@ -2,6 +2,28 @@
 
 #include "pin_config.h"
 #include "pinout.h"
+#include <stdio.h>
+#include <string.h>
+
+
+#define NUM_ROWS 2
+#define NUM_COLS 16
+
+#define ROW0_OFFSET 0x00
+#define ROW1_OFFSET 0x40
+
+#define CLEAR_DISPLAY     0x01
+#define RETURN_HOME       0x02
+#define ENTRY_SET_DEF_LAT 0x06
+#define DISPLAY_OFF       0x08
+#define DISPLAY_ON        0x0C
+#define FUNCTION_SET_4B1L 0x20
+#define FUNCTION_SET_4B2L 0x28
+#define FUNCTION_SET_8B1L 0x30
+#define SET_DDRAM_ADDR    0x80
+
+
+static const uint8_t ROW_OFFSETS[] = {ROW0_OFFSET, ROW1_OFFSET};
 
 static void forward_data(void);
 static void init_contrast(void);
@@ -65,6 +87,23 @@ void lcd1602a_clear(void)
 	send_command(CLEAR_DISPLAY);
 }
 
+void lcd1602_clear_row(uint8_t row)
+{
+	// N.B. the HD44780U does not support
+	// single row clears, so this just prints
+	// spaces, which is suboptimal
+	lcd1602a_set_cursor(row, 0);
+	for(size_t idx = 0; idx < NUM_COLS; idx++)
+	{
+		lcd1602a_putchar(' ');
+	}
+}
+
+void lcd1602a_set_cursor(uint8_t x, uint8_t y)
+{
+	send_command(SET_DDRAM_ADDR | ((y % NUM_COLS) + ROW_OFFSETS[x % NUM_ROWS]));
+}
+
 
 void lcd1602a_putchar(unsigned char c)
 {
@@ -79,15 +118,25 @@ void lcd1602a_putchar(unsigned char c)
 	delay_us(2000);
 }
 
-void lcd1602a_print(const char *str, size_t len)
+void lcd1602a_print(const char *str)
 {
 	lcd1602a_clear();
-	for(size_t idx = 0; idx < len; idx++)
+	lcd1602a_print_row(str, 0);
+}
+
+void lcd1602a_print_row(const char *str, uint8_t row)
+{
+	// TODO maybe an option to clear only if the user wants
+	size_t len = strlen(str);
+	size_t idx = 0;
+
+	lcd1602_clear_row(row);
+	lcd1602a_set_cursor(row, 0);
+	for(; (idx < len) && (idx < NUM_COLS); idx++)
 	{
 		lcd1602a_putchar(str[idx]);
 	}
 }
-
 
 static void send_command(unsigned char cmd)
 {
--- 1602a_lcd/1602a_lcd.h
+++ 1602a_lcd/1602a_lcd.h
@@ -1,25 +1,18 @@
 #ifndef AY38910A_SYNTH_1602A_LCD_H
 #define AY38910A_SYNTH_1602A_LCD_H
 
-#include <stdio.h>
-
-#define CLEAR_DISPLAY     0x01
-#define RETURN_HOME       0x02
-#define ENTRY_SET_DEF_LAT 0x06
-#define DISPLAY_OFF       0x08
-#define DISPLAY_ON        0x0C
-#define FUNCTION_SET_4B1L 0x20
-#define FUNCTION_SET_4B2L 0x28
-#define FUNCTION_SET_8B1L 0x30
-
+#include <stdint.h>
 
 void lcd1602a_init(void);
 void lcd1602a_display_on(void);
 void lcd1602a_display_off(void);
 void lcd1602a_home(void);
 void lcd1602a_clear(void);
+void lcd1602_clear_row(uint8_t row);
+void lcd1602a_set_cursor(uint8_t x, uint8_t y);
 void lcd1602a_putchar(unsigned char c);
-void lcd1602a_print(const char *str, size_t len);
+void lcd1602a_print(const char *str);
+void lcd1602a_print_row(const char *str, uint8_t row);
 void delay_us(uint16_t us);
 void delay_ms(uint16_t ms);
 #endif
