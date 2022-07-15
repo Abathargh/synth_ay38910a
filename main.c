82ad985a01e624292d173578085410a01f87ef21
mar <gim.marcello@gmail.com>
2022-07-15 22:22:18 +0200

    fixed: bug in the unused channels cleanup loop, tone channels enabling; changed: comments and partial cleanup.

--- main.c
+++ main.c
@@ -7,34 +7,34 @@
 int main(void)
 {
 	ay38910_init();
-	ay38910_channel_mode(CHA_TONE_ENABLE);
-	ay38910_set_amplitude(CHANNEL_A, 15);
+	ay38910_channel_mode(CHA_TONE_ENABLE | CHB_TONE_ENABLE | CHC_TONE_ENABLE);
 	
 	keyboard_init();
 
 	uint8_t mask = 0;
-	uint8_t octave = 4;
+	uint8_t octave = 4; // octave is fixed to 4 for testing purposes
 	
 	while(1)
 	{
 		if(keyboard_acquire(&mask))
 		{
 			char count = 0;
-			for(uint8_t i = 0; (i < sizeof(mask) * 8) && count <=3; i++)
+			for(uint8_t i = 0; (i < 8) && (count <= 3); i++)
 			{
 				if(mask & (1 << i))
 				{
-					Channel chan = (Channel)(2*count); // TODO define as macro?
+					// volatile to prevent optimizations in debug mode
+					volatile Channel chan = (Channel)(2*count);
 					ay38910_set_amplitude(chan, 15);
 					ay38910_play_note(chan, NOTE(1+i, octave));
 					count++;
 				}
 			}
 			
-			// Close unused channels due to a key not being pressed anymore
+			// close unused channels due to a key not being pressed anymore
 			for(uint8_t ch = count ; ch < CHANNEL_NUM; ch++)
 			{
-				ay38910_set_amplitude((Channel)ch, 0);
+				ay38910_set_amplitude((Channel)(2*ch), 0);
 			}
 		}
 		delay_ms(2);
