6717e08b6382039838aa3eaf529e417d8575d3b8
mar <gim.marcello@gmail.com>
2022-07-17 23:57:56 +0200

    changed: cleaned magic numbers and fixed the initiali logic of the key acquisition; fixed channel enabling/disabling by pushing up state in the main and keeping it updated there.

--- keyboard_ifc/keyboard_ifc.c
+++ keyboard_ifc/keyboard_ifc.c
@@ -9,37 +9,32 @@
 #define ROW_NUM 4
 #define COL_NUM 3
 
-#define ROW(n) keyboard_out,(2+n)
+#define COL_MASK 0x07 // 3 cols => 00000111
+#define ROW_MASK 0x0F // 4 rows => 00001111
+
+#define ROW(n) keyboard_out,n
 
 
 void keyboard_init(void)
 {
-	InitPort(keyboard_out, 0x0F);
-
-	// Set the keyboard cols as pullup inputs
-	InitPort(keyboard_in, 0x07);
-	SetPort(keyboard_in, 0x03); 
-
-	SetPort(keyboard_out, 0x0F);
+	InitOrPort(keyboard_out, ROW_MASK);
+	InitAndPort(keyboard_in, ~COL_MASK);
+	
+	SetPort(keyboard_out, ROW_MASK);
+	SetLoPort(keyboard_in, COL_MASK);
 }
 
 bool keyboard_acquire(uint16_t *mask)
 {
-	uint8_t cur_mask = 0;
-	uint8_t columns = GetPort(keyboard_in) & 0x03;
+	uint16_t cur_mask = 0;
+	uint8_t columns;
 	
-	while(columns == 0x03)
-	{
-		delay_ms(2);
-		columns = GetPort(keyboard_in) & 0x03;
-	}
-
 	for(int i = 0; i < ROW_NUM; i++)
 	{
 		// ground one by one to check for key presses
 		ResetPin(ROW(i));
 		delay_ms(2);
-		columns = GetPort(keyboard_in) & 0x03;
+		columns = GetPort(keyboard_in) & COL_MASK;
 		
 		for(int j = 0; j < COL_NUM; j++)
 		{
--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include "1602a_lcd.h"
 #include "delay.h"
 #include "Ay38910a.h"
 #include "keyboard_ifc.h"
@@ -8,33 +9,40 @@ int main(void)
 {
 	ay38910_init();
 	ay38910_channel_mode(CHA_TONE_ENABLE | CHB_TONE_ENABLE | CHC_TONE_ENABLE);
-	
+
 	keyboard_init();
 
 	uint16_t mask   = 0;
-	uint8_t  octave = 4; // octave is fixed to 4 for testing purposes
+	uint8_t  octave = 2; // octave is fixed to 4 for testing purposes
+	uint8_t  channel_mask = 0;
 	
 	while(1)
 	{
 		if(keyboard_acquire(&mask))
 		{
-			char count = 0;
-			for(uint8_t i = 0; (i < KEYBOARD_KEYS) && (count <= CHANNEL_NUM); i++)
+			uint8_t count = 0;
+			for(uint8_t i = 0; (i < KEYBOARD_KEYS) && (count < CHANNEL_NUM); i++)
 			{
 				if(mask & (1 << i))
 				{
-					// volatile to prevent optimizations in debug mode
-					volatile Channel chan = (Channel)(2*count);
+					// With noise channels, this works with count+OFFSET
+					channel_mask &= ~(1 << count);
+					Channel chan = (Channel)(2*count);
+					ay38910_channel_mode(channel_mask);
 					ay38910_set_amplitude(chan, 15);
 					ay38910_play_note(chan, NOTE(1+i, octave));
 					count++;
 				}
 			}
 			
+			// for is called anyways 
 			// close unused channels due to a key not being pressed anymore
 			for(uint8_t ch = count ; ch < CHANNEL_NUM; ch++)
 			{
-				ay38910_set_amplitude((Channel)(2*ch), 0);
+				channel_mask |= (1 << count);
+				Channel chan = (Channel)(2*ch);
+				ay38910_channel_mode(channel_mask);
+				ay38910_set_amplitude(chan, 0);
 			}
 		}
 		delay_ms(2);
