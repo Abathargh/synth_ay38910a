a9812be3ccfd2eb4879db35ddd06d9bf1651d840
mar <gim.marcello@gmail.com>
2022-12-25 18:12:52 +0100

    fixed: added quote escapes in setup script.

--- setup.sh
+++ setup.sh
@@ -6,9 +6,9 @@ targets=(atmega644 atmega2560)
 prog_str=("atmelice" "stk500v2 -D -b 115200 -P $SERIAL_PORT")
 
 
-for i in ${!targets[@]}; do
-  rm -r build-${targets[$i]}
-  mkdir build-${targets[$i]}
-  cmake -S . -B build-${targets[$i]} \
-    -DMCU=${targets[$i]} -DAVRDUDE_PRG_STR="${prog_str[$i]}"
+for i in "${!targets[@]}"; do
+  rm -r build-"${targets[$i]}"
+  mkdir build-"${targets[$i]}"
+  cmake -S . -B build-"${targets[$i]}" \
+    -DMCU="${targets[$i]}" -DAVRDUDE_PRG_STR="${prog_str[$i]}"
 done
