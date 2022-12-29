d9340efb008635a1ead931b2f7695fd507e9b366
mar <gim.marcello@gmail.com>
2022-12-29 13:10:03 +0100

    added: delay docstrings. changed: fixed inner cycle length within the millisecond delay routine.

--- inc/delay.h
+++ inc/delay.h
@@ -1,3 +1,9 @@
+/** @file delay.h
+ *
+ * This module implements custom delay routines.
+ * These delay aims to be as precise as possible, and are implemented in
+ * assembly.
+ */
 #ifndef AY38910A_SYNTH_DELAY_LCD_H
 #define AY38910A_SYNTH_DELAY_LCD_H
 
@@ -11,7 +17,30 @@
 /* Defines                                                              */
 /************************************************************************/
 
+/**
+ * @brief Performs a blocking delay with a microsecond granularity
+ *
+ * This delay has a small error of exactly one cycle and is best used
+ * when a specific blocking delay with microsecond granularity is needed,
+ * for example when writing peripheral drivers.
+ *
+ * It only works with a 16MHz clock source.
+ *
+ * @param us the number of microseconds to wait
+ */
 void delay_us(uint16_t us);
+
+/**
+ * @brief Performs a blocking delay with a millisecond granularity
+ *
+ * This delay has a small error of n microseconds + 1, and is best used
+ * when a specific blocking delay with milliseconds granularity is needed,
+ * for example when writing peripheral drivers.
+ *
+ * It only works with a 16MHz clock source.
+ *
+ * @param ms the number of milliseconds to wait
+ */
 void delay_ms(uint16_t ms);
 
 #endif
--- src/delay.c
+++ src/delay.c
@@ -1,13 +1,3 @@
-/**
- * delay.c
- *
- * This module implements custom delay routines.
- * These delay aims to be as precise as possible, and are implemented in
- * inline assembly.
- *
- * Author: mar
- */
-
 /************************************************************************/
 /* Includes                                                             */
 /************************************************************************/
@@ -16,15 +6,24 @@
 
 #include <stdint.h>
 
-/**
- * Performs a blocking delay with a microsecond granularity
- * @param us the number of microseconds to wait
- */
+/************************************************************************/
+/* Function implementations                                             */
+/************************************************************************/
+
 inline __attribute__((always_inline))
 void delay_us(uint16_t us)
 {
-	// The small error is due to using a 16 bit input
-	// T_del = 	T_clk * #cycles = (n + 0,0625) us
+	/**
+	 * Blocking delay that is hand written around a 16MHz clock source.
+	 * The internal loop is exactly 16 clock cycles long. In the last
+	 * iteration, when the loop breaks, the last BRNE that gets executed
+	 * only needs 1 clock cycle. If we consider the last BRNE together with
+	 * one of the first MOV, then we can observe that this delay executes
+	 * only one cycle more than it should, giving a delay of:
+	 *   T_del = 	T_clk * #cycles = (n + 0,0625) us
+	 * Note that this implementation is inlined, so there should not be any
+	 * additional function call overhead.
+	 */
 	asm volatile(
 		"MOV ZH,%B0\n\t"  // MOV: 1 cycle
 		"MOV ZL,%A0\n\t"  // MOV: 1 cycle
@@ -41,8 +40,8 @@ void delay_us(uint16_t us)
 		"NOP\n\t"
 		"NOP\n\t"
 		"NOP\n\t"
-		"SBIW Z,1\n\t"
-		"BRNE %=b\n\t"
+		"SBIW Z,1\n\t"    // SBIW: 2 cycles
+		"BRNE %=b\n\t"    // BRNE: 2 if condition is false, 1 otherwise
 		:
 		: "r" (us)
 	);
@@ -56,20 +55,35 @@ inline __attribute__((always_inline))
 void delay_ms(uint16_t ms)
 {
 	/**
+	 * Blocking delay that is hand written around a 16MHz clock source.
+	 * The approach is similar to the microsecond delay, but with some
+	 * more loops. Let us consider the inner loop as having varying
+	 * number of cycles 'N'. Then, by following a similar approach in
+	 * counting cycles together as for the previous delay, we come to
+	 * the following:
+	 *   #cycles = n(1000N + 16) + 1
+	 * Where 'n' is the number of repetitions passed by the user (the
+	 * 'ms' parameter). Dividing everything by the clock frequency yields
+	 * the actual elapsed time in seconds:
+	 *   T_del(n) = #cycles/f_clk = n(0,0625N ms + 0,001 ms) + 1 us
+	 * That, with N = 16, is equal to:
+	 *   T_del(n) = n * 1,001 ms + 0,001 ms
+	 * Note that this implementation is inlined, so there should not be any
+	 * additional function call overhead.
+	 *
 	 * TODO inform the compiler via clobber that the registers are used and check
 	 * if it works refer to inline assembly guide optimization => try to remove
 	 * MOVs since R25:R24 should be used as first input register
 	 */
-	// TODO change with info on ms delay T_del = 	T_clk * #cycles = (n + 0,0625) us
-	
 	asm volatile(
 		"MOV ZH,%B0\n\t"  // MOV: 1 cycle
-		"MOV ZL,%A0\n\t"  // MOV: 1 cycle => 1 + (16016 + 4) * ms = 16020 * ms + 1
-		"OUTER%=:\n\t"    // (4000 + 4) * 4 = 16016
+		"MOV ZL,%A0\n\t"  // MOV: 1 cycle => 1 + (16012 + 4) * ms = rep*16016 + 1
+		"OUTER%=:\n\t"    // (4000 + 3) * 4 = 16012
 		"LDI R18,4\n\t"   // LDI: 1 cycle
 		"MILLISEC%=:\n\t" // 16 * 250 = 4000 cycles
 		"LDI R17,250\n\t" // LDI: 1 cycle
 		"MICROSEC%=:\n\t" // MICROSEC LOOP: 16 cycles tot (including previous LDI per cycle)
+		"NOP\n\t"
 		"NOP\n\t"
 		"NOP\n\t"
 		"NOP\n\t"
@@ -87,7 +101,7 @@ void delay_ms(uint16_t ms)
 		"DEC R18\n\t"
 		"BRNE MILLISEC%=\n\t"
 		"SBIW Z,1\n\t"        // SBIW: 2 cycles
-		"BRNE OUTER%=\n\t"
+		"BRNE OUTER%=\n\t"    // BRNE: 2 if condition is false, 1 otherwise
 		:
 		: "r" (ms)
 	);
