5337fe02a0512558371a3b843491061ba479ce1d
mar <gim.marcello@gmail.com>
2022-12-30 17:56:02 +0100

    added: docstrings for the logger module. changed: block formatting.

--- inc/logger.h
+++ inc/logger.h
@@ -1,42 +1,53 @@
+/** @file logger.h
+ *
+ * This module implements a basic logger over USART.
+ */
 
 #ifndef LOGGER_H_
 #define LOGGER_H_
 
 /************************************************************************/
-/* Includes                                                             */
-/************************************************************************/
-
-/************************************************************************/
 /* Defines                                                              */
 /************************************************************************/
-
-/**
- * The baud rate formula was taken from section 22.5 of the ATMega2560 
- * datasheet, and is based on having the F_CPU macro at disposal. This 
- * formula works for the ATMega644 too.
- */
 
 #ifndef F_CPU
 #warning The logger module requires to define F_CPU, defaulting to 16MHz
 #define F_CPU 16000000
 #endif
 
+/**
+ * The baud rate formula was taken from section 22.5 of the ATMega2560
+ * datasheet, and is based on having the F_CPU macro at disposal. This
+ * formula works for the ATMega644 too.
+ */
 #define BAUD_VAL(b) (F_CPU / 16 / b - 1)
 
 /************************************************************************/
 /* Typedefs                                                             */
 /************************************************************************/
 
+/**
+ * @brief Supported baud rates
+ */
 typedef enum {
-  BAUD_RATE_9600  = BAUD_VAL(9600),
-  BAUD_RATE_38400 = BAUD_VAL(38400)
+  BAUD_RATE_9600  = BAUD_VAL(9600), /**< 9600 baud */
+  BAUD_RATE_38400 = BAUD_VAL(38400) /**< 38400 baud */
 } baudrate_t;
 
 /************************************************************************/
 /* Public functions                                                     */
 /************************************************************************/
 
+/**
+ * @brief Initializes the USART perpipheral used by the logger module
+ * @param baud one of the supported baud rates
+ */
 void logger_init(baudrate_t baud);
+
+/**
+ * @brief Prints a message through the initialized USART
+ * @param msg the biffer containing the message
+ */
 void logger_print(const char *msg);
 
 #endif
--- src/logger.c
+++ src/logger.c
@@ -1,11 +1,3 @@
-/**
- * logger.c
- *
- * This module implements a basic logger over USART.
- *
- * Author: mar
- */
-
 /************************************************************************/
 /* Includes                                                             */
 /************************************************************************/
@@ -25,87 +17,75 @@
 #if defined(__AVR_ATmega2560__)
 #define LOGGER_PORT 3
 #elif defined(__AVR_ATmega644__)
-#define LOGGER_PORT 0 
+#define LOGGER_PORT 0
 #endif
 
 #endif
 
 /**
  * Generic register macros used as a general approach
- * to selecting the available registers depending on 
+ * to selecting the available registers depending on
  * MCU currently selected as the target.
  *
- * These can be overridden to use a specifict USART 
+ * These can be overridden to use a specifict USART
  * register by defining the 'LOGGER_PORT' macro.
  *
  */
 
-#define __UBRRH(x) UBRR ## x ## H
-#define __UBRRL(x) UBRR ## x ## L
-#define __UCSRA(x) UCSR ## x ## A
-#define __UCSRB(x) UCSR ## x ## B
-#define __UCSRC(x) UCSR ## x ## C
-#define __UCSZ2(x) UCSZ ## x ## 2
-#define __UCSZ1(x) UCSZ ## x ## 1
-#define __UCSZ0(x) UCSZ ## x ## 0
-#define __UDR(x)   UDR  ## x
-#define __UDRE(x)  UDRE ## x
-#define __TXEN(x)  TXEN ## x
+#define UBRRH__(x) UBRR ## x ## H
+#define UBRRL__(x) UBRR ## x ## L
+#define UCSRA__(x) UCSR ## x ## A
+#define UCSRB__(x) UCSR ## x ## B
+#define UCSRC__(x) UCSR ## x ## C
+#define UCSZ2__(x) UCSZ ## x ## 2
+#define UCSZ1__(x) UCSZ ## x ## 1
+#define UCSZ0__(x) UCSZ ## x ## 0
+#define UDR__(x)   UDR  ## x
+#define UDRE__(x)  UDRE ## x
+#define TXEN__(x)  TXEN ## x
 
-#define _UBRRH(x) __UBRRH(x) 
-#define _UBRRL(x) __UBRRL(x) 
-#define _UCSRA(x) __UCSRA(x) 
-#define _UCSRB(x) __UCSRB(x) 
-#define _UCSRC(x) __UCSRC(x) 
-#define _UCSZ2(x) __UCSZ2(x) 
-#define _UCSZ1(x) __UCSZ1(x) 
-#define _UCSZ0(x) __UCSZ0(x) 
-#define _UDR(x)   __UDR(x)
-#define _UDRE(x)  __UDRE(x)
-#define _TXEN(x)  __TXEN(x)
+#define UBRRH_(x) UBRRH__(x)
+#define UBRRL_(x) UBRRL__(x)
+#define UCSRA_(x) UCSRA__(x)
+#define UCSRB_(x) UCSRB__(x)
+#define UCSRC_(x) UCSRC__(x)
+#define UCSZ2_(x) UCSZ2__(x)
+#define UCSZ1_(x) UCSZ1__(x)
+#define UCSZ0_(x) UCSZ0__(x)
+#define UDR_(x)   UDR__(x)
+#define UDRE_(x)  UDRE__(x)
+#define TXEN_(x)  TXEN__(x)
 
-#define UBRRH _UBRRH(LOGGER_PORT)
-#define UBRRL _UBRRL(LOGGER_PORT)
-#define UCSRA _UCSRA(LOGGER_PORT)
-#define UCSRB _UCSRB(LOGGER_PORT)
-#define UCSRC _UCSRC(LOGGER_PORT)
-#define UCSZ2 _UCSZ2(LOGGER_PORT)
-#define UCSZ1 _UCSZ1(LOGGER_PORT)
-#define UCSZ0 _UCSZ0(LOGGER_PORT)
-#define UDR   _UDR(LOGGER_PORT)
-#define UDRE  _UDRE(LOGGER_PORT)
-#define TXEN  _TXEN(LOGGER_PORT)
+#define UBRRH UBRRH_(LOGGER_PORT)
+#define UBRRL UBRRL_(LOGGER_PORT)
+#define UCSRA UCSRA_(LOGGER_PORT)
+#define UCSRB UCSRB_(LOGGER_PORT)
+#define UCSRC UCSRC_(LOGGER_PORT)
+#define UCSZ2 UCSZ2_(LOGGER_PORT)
+#define UCSZ1 UCSZ1_(LOGGER_PORT)
+#define UCSZ0 UCSZ0_(LOGGER_PORT)
+#define UDR   UDR_(LOGGER_PORT)
+#define UDRE  UDRE_(LOGGER_PORT)
+#define TXEN  TXEN_(LOGGER_PORT)
 
 
 /************************************************************************/
 /* Function implementations                                             */
 /************************************************************************/
 
-/**
- * Initializes the USART perpipheral used by the logger module
- * 
- * @param baud one of the supported baud rates
- */
 void logger_init(baudrate_t baud)
 {
-  UBRRH = (uint8_t)(baud >> 8); 
+  UBRRH = (uint8_t)(baud >> 8);
   UBRRL = (uint8_t) baud;
-  UCSRB = (1 << TXEN);  
+  UCSRB = (1 << TXEN);
 
   // 8-bit 1-stop bit
-  UCSRC = (1 << UCSZ2) | (1 << UCSZ1) | (1 << UCSZ0);  
+  UCSRC = (1 << UCSZ1) | (1 << UCSZ0);
 }
 
-
-/**
- * Prints a message through the initialized USART
- * 
- * @param msg the biffer containing the message
- */
 void logger_print(const char *msg)
 {
-  while(*msg)
-  {
+  while(*msg) {
     // Wait for the TX buffer to be empty
     while (!(UCSRA & (1 << UDRE)));
     UDR = *msg;
