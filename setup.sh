5a61e0adc0426d96c352ba84f9422788be35ca7e
mar <gim.marcello@gmail.com>
2023-01-03 01:24:57 +0100

    changed: setup script handles debug/release cmake initialization.

--- setup.sh
+++ setup.sh
@@ -1,18 +1,29 @@
 #!/usr/bin/env bash
 
-targets=(atmega644 atmega2560)
+set -x
 
-if [ $# -ne 1 ]; then
-  echo "setup-cmake expects one parameter."
-  echo "Usage: python cmake-setup.py [target]"
-  exit 1
-fi
+targets=(atmega644 atmega2560)
+release="-DCMAKE_BUILD_TYPE=Debug"
+
+while getopts "r" opt; do
+  case "$opt" in
+    r)  release="-DCMAKE_BUILD_TYPE=Release";;
+    \?) printf "illegal option: -%s\n" "$OPTARG" >&2
+        exit 1 ;;
+    :)  printf "missing argument for -%s\n" "$OPTARG" >&2
+        exit 1;;
+  esac
+done
+
+
+shift $(($OPTIND - 1))
+passed_target=$1
 
 for target in "${targets[@]}"; do
-  if [ "$target" == "$1" ]; then
+  if [ "$target" == "$passed_target" ]; then
     rm -rf build
     mkdir build
-    cmake -S . -B build -DMCU="$target"
+    cmake -S . -B build -DMCU="$target" "$release"
     exit 0
   fi
 done
